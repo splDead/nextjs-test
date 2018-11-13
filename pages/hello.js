a25d799a4d689b144b10685c179337c71386e7f4
Sergey Goryachev <spldead@gmail.com>
2018-11-13 21:51:30 +0300

    knock

--- pages/hello.js
+++ pages/hello.js
@@ -3,5 +3,8 @@ import Layout from '../components/Layout';
 export default () => (
     <Layout>
         <p>Hello world</p>
+        <div>
+            knock knock neo
+        </div>
     </Layout>
 );
